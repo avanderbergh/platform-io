f767a1214794bc0320e8cb5899f3ca75e58113a5
Adriaan van der Bergh <avanderbergh@gmail.com>
2024-10-21 12:16:47 +0200

    Testing AsyncWebServer on the M5Stack

--- m5stack-server/src/main.cpp
+++ m5stack-server/src/main.cpp
@@ -1,18 +1,71 @@
 #include <Arduino.h>
+#include <ArduinoJson.h>
+#include <AsyncJson.h>
+#include <ESPAsyncWebServer.h>
+#include <M5Stack.h>
+#include <WiFi.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define XSTR(x) #x
+#define STR(x) XSTR(x)
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+const char *WIFI_SSID = STR(WIFI_SSID);
+const char *WIFI_PASSWORD = STR(WIFI_PASSWORD);
+
+float ax, ay, az;
+
+AsyncWebServer server(80);
+
+void setup()
+{
+  delay(2000);
+  Serial.begin(9600);
+  delay(2000);
+  Serial.printf("Connecting to %s\n", WIFI_SSID);
+
+  WiFi.disconnect();
+  delay(100);
+  WiFi.setHostname("nibbler");
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+
+    if (WiFi.status() == WL_CONNECT_FAILED)
+    {
+      Serial.println("Connection failed");
+      return;
+    }
+  }
+
+  Serial.println("Connected");
+  Serial.printf("IP address: %s", WiFi.localIP().toString().c_str());
+
+  M5.begin();
+  M5.Lcd.print(WiFi.localIP());
+
+  M5.Imu.Init();
+
+  server.on("/gyro", HTTP_GET, [](AsyncWebServerRequest *request)
+            {
+              Serial.println("Request received");
+              M5.Imu.getAccelData(&ax, &ay, &az);
+
+              AsyncResponseStream *response = request->beginResponseStream("application/json");
+
+              JsonDocument doc;
+              doc["x"] = ax;
+              doc["y"] = ay;
+              doc["z"] = az;
+
+              serializeJson(doc, *response);
+
+              request->send(response); });
+
+  server.begin();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop()
+{
 }
