631a474bb6ca56b68d8c6bbe93f40e54720104e3
Adriaan van der Bergh <avanderbergh@gmail.com>
2024-10-21 16:01:32 +0200

    Add M5StickC server project

---
+++ m5stick-server/src/main.cpp
@@ -0,0 +1,63 @@
+#include <Arduino.h>
+#include <ESPAsyncWebServer.h>
+#include <M5StickC.h>
+#include <WiFi.h>
+
+#include "DHT12.h"
+
+#define XSTR(x) #x
+#define STR(x) XSTR(x)
+
+DHT12 dht12;
+
+const char *WIFI_SSID = STR(WIFI_SSID);
+const char *WIFI_PASSWORD = STR(WIFI_PASSWORD);
+
+AsyncWebServer server(80);
+
+void setup()
+{
+  M5.begin();
+  Wire.begin(0, 26);
+
+  M5.Lcd.setRotation(3);
+  M5.Lcd.fillScreen(BLACK);
+  M5.Lcd.setTextColor(WHITE);
+  M5.Lcd.setTextPadding(20);
+  M5.Lcd.setCursor(20, 20);
+
+  WiFi.disconnect();
+  delay(100);
+  WiFi.setHostname("zapp");
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+
+    if (WiFi.status() == WL_CONNECT_FAILED)
+    {
+      Serial.println("Connection failed");
+      return;
+    }
+  }
+
+  M5.Lcd.print(WiFi.localIP());
+
+  server.on("/tmp", HTTP_GET, [](AsyncWebServerRequest *request)
+            {
+              float tmp = dht12.readTemperature();
+              request->send(200, "text/plain", String(tmp)); });
+
+  server.on("/hum", HTTP_GET, [](AsyncWebServerRequest *request)
+            {
+              float hum = dht12.readHumidity();
+              request->send(200, "text/plain", String(hum)); });
+
+  server.begin();
+}
+
+void loop()
+{
+}
